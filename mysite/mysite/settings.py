8002495422449f203b62a120b4e23b1c3dac1d89
John McBride <john.e.mcbride@icloud.com>
2024-08-30 21:19:37 +0100

    trying to fix the frame thing

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
     'inertia.middleware.InertiaMiddleware',
